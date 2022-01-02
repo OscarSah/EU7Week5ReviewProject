7f856f21467e8fc82e37640421742ae7c0b110da
OscarSah <oscar@cybertekschool.com>
2022-01-02 19:17:31 +0300

    Task 2 completed

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,6 +21,12 @@ public class ChercherTest {
     2. Click on "Click me, to Open an alert after 5 seconds"
     3. Explicitly wait until alert is present
     4. Then handle the Javascript alert
+
+     Task2:
+    1. Go to https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver
+    2. Click on "Enable button after 10 seconds"
+    3. Explicitly wait until the button is enabled
+    4. Then verify the button is enabled
  */
     WebDriver driver; // declare our reference for the object
 
@@ -50,6 +57,21 @@ public class ChercherTest {
         // if you don't wait you will get NoAlertPresentException: no such alert
         Alert alert = driver.switchTo().alert();
         alert.accept();
+    }
+
+    @Test
+    public void disabledButtonTest(){
+        WebElement button = driver.findElement(By.id("disable"));
+        System.out.println("button.isEnabled() = " + button.isEnabled()); // false
+
+        WebElement buttonInitiator = driver.findElement(By.id("enable-button"));
+        buttonInitiator.click();
+
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        System.out.println("button.isEnabled() = " + button.isEnabled()); // true
+        Assert.assertTrue(button.isEnabled(),"Verify the Button is enabled");
     }
 
 }
